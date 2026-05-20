564bae3650fabce2dfc6838d41f79c8d3504c181
khalidalsatoof <klonve2011@hotmail.com>
2026-05-20 22:00:34 +0300

    Delete layout.js

--- layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: 'SignalBot', description: 'NASDAQ Trading Signals' }
-export default function RootLayout({ children }) {
-  return (
-    <html lang="ar" dir="rtl">
-      <body style={{margin:0,padding:0,background:'#050810'}}>{children}</body>
-    </html>
-  )
-}
